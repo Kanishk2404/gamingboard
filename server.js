1c7393cfd7271049ea332c765bd75de30370f7f6
kanishk2404 <saraswatkanishk24.com>
2025-02-13 23:29:36 +0530

    fijj

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 
 app.get("/top-scores", (req, res) => {
-  res.json({ scores: [100, 95, 90, 85, 190, 120, 180, 200, 500, 60,90,9876543] });
+  res.json({ scores: [100, 95, 90, 85, 190, 120, 180, 200, 500, 60, 90, 9876543] });
 });
 
-app.listen(3000, () => console.log("Leaderboard service running on port 3000"));
+const PORT = process.env.PORT || 3000;
+const HOST = '0.0.0.0';
+
+app.listen(PORT, HOST, () => console.log(`Leaderboard service running on http://${HOST}:${PORT}`));
 
 module.exports = app;
